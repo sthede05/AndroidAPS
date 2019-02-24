218486435951851f62a5ec81619ded1e528c4c7a
Baris Kurtlutepe <barisk@gmail.com>
2019-02-24 18:31:49 +0100

    Added omnipy_omnipod as pump type

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpCommon/defs/PumpCapability.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpCommon/defs/PumpCapability.java
@@ -22,6 +22,7 @@ public enum PumpCapability {
     DanaCapabilities(Bolus, ExtendedBolus, TempBasal, BasalProfileSet, Refill, TDD, ManualTDDLoad), //
     DanaWithHistoryCapabilities(Bolus, ExtendedBolus, TempBasal, BasalProfileSet, Refill, StoreCarbInfo, TDD, ManualTDDLoad), //
     InsightCapabilities(Bolus, ExtendedBolus, TempBasal, BasalProfileSet, Refill,TDD,BasalRate30min), //
+    OmnipyOmnipodCapabilities(Bolus, TempBasal, BasalRate30min),
 
 
     // BasalRates (separately grouped)
