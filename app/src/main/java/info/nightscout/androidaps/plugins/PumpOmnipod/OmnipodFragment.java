b68569c1a2e3d3b0c8a01a3f2109ed16e540c402
Baris Kurtlutepe <barisk@gmail.com>
2019-02-24 14:35:26 +0100

    fragment logic changed to work the aaps way

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/OmnipodFragment.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/OmnipodFragment.java
@@ -4,25 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.otto.Subscribe;
+
 import info.nightscout.androidaps.R;
 import info.nightscout.androidaps.plugins.Common.SubscriberFragment;
+import info.nightscout.androidaps.plugins.PumpVirtual.events.EventVirtualPumpUpdateGui;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link OmnipodFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link OmnipodFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OmnipodFragment extends SubscriberFragment {
-
-    private OnFragmentInteractionListener mListener;
 
     public OmnipodFragment() {
         // Required empty public constructor
@@ -37,67 +31,32 @@ public class OmnipodFragment extends SubscriberFragment {
         super.onCreate(savedInstanceState);
 
         Intent intent = getActivity().getIntent();
+        if (intent != null) {
 
-        Bundle extras = intent.getExtras();
-        if (extras != null)
-        {
-            String address = extras.getString("omnipy_address");
-            if (address != null)
-            {
-                //
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                String address = extras.getString("omnipy_address");
+                if (address != null) {
+                    //
+                }
             }
         }
+    }
 
+    @Subscribe
+    public void onStatusEvent(final EventVirtualPumpUpdateGui ev) {
+        updateGUI();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_omnipod, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
     @Override
     protected void updateGUI() {
 
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
