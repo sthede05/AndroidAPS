5adda63d56df4f3053e849a993238ab67f255782
Baris Kurtlutepe <barisk@gmail.com>
2019-03-03 11:24:31 +0100

    self rescheduling fix

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/OmnipyNetworkDiscovery.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/OmnipyNetworkDiscovery.java
@@ -52,6 +52,13 @@ public class OmnipyNetworkDiscovery {
     public void SetAddress(String address) {
         _lastKnownAddress = address;
         _discovering = false;
+        if (address != null) {
+            _log.debug("Omnipy address discovered: %s", address);
+        }
+        else
+        {
+            _log.debug("Omnipy address not found", address);
+        }
 //        Intent intent = new Intent(_context, OmnipodFragment.class);
 //        intent.getExtras().putString("omnipy_address", address);
 //        _context.startActivity(intent);
@@ -74,6 +81,7 @@ class OmnipyNetworkDiscoveryTask extends TimerTask {
     private OmnipyUDPBroadcastTask _broadcastTask;
     private Context _context;
     private OmnipyNetworkDiscovery _mainDiscovery;
+    private Logger _log;
 
     public OmnipyNetworkDiscoveryTask(OmnipyUDPBroadcastTask broadcastTask, Context context,
                                       OmnipyNetworkDiscovery mainDiscovery)
@@ -81,12 +89,15 @@ class OmnipyNetworkDiscoveryTask extends TimerTask {
         _broadcastTask = broadcastTask;
         _context = context;
         _mainDiscovery = mainDiscovery;
+        _log = LoggerFactory.getLogger(L.PUMP);
     }
 
     @Override
     public void run() {
+
         if (_broadcastTask == null)
         {
+            _log.debug("Creating new broadcast task");
             _broadcastTask = new OmnipyUDPBroadcastTask(_context);
             _broadcastTask.execute();
             this.reschedule(10000);
@@ -95,6 +106,7 @@ class OmnipyNetworkDiscoveryTask extends TimerTask {
         {
             if (_broadcastTask.getStatus() == AsyncTask.Status.FINISHED)
             {
+                _log.debug("Broadcast task finished, getting results");
                 String address = null;
                 try {
                     address = _broadcastTask.get();
@@ -109,15 +121,18 @@ class OmnipyNetworkDiscoveryTask extends TimerTask {
 
                 if (address != null)
                 {
+                    _log.debug("Broadcast task found address %s", address);
                     _mainDiscovery.SetAddress(address);
                 }
                 else
                 {
+                    _log.debug("Broadcast task returned no result");
                     this.reschedule(60000);
                 }
             }
             else
             {
+                _log.debug("Broadcast task still running");
                 this.reschedule(5000);
             }
         }
@@ -125,6 +140,7 @@ class OmnipyNetworkDiscoveryTask extends TimerTask {
 
     private void reschedule(long delay)
     {
+        _log.debug("Rescheduling task status check in %d seconds", delay / 1000);
         Timer timer = new Timer("OmnipyNetworkDiscovery", true);
         timer.schedule(new OmnipyNetworkDiscoveryTask(_broadcastTask, _context, _mainDiscovery) , delay);
     }
@@ -134,11 +150,13 @@ class OmnipyNetworkDiscoveryTask extends TimerTask {
 class OmnipyUDPBroadcastTask extends AsyncTask<Void, Void, String> {
 
     Context _context;
+    Logger _log;
 
     public OmnipyUDPBroadcastTask(Context context)
     {
         super();
         _context = context;
+        _log = LoggerFactory.getLogger(L.PUMP);
     }
 
     @Override
@@ -168,15 +186,23 @@ class OmnipyUDPBroadcastTask extends AsyncTask<Void, Void, String> {
             DatagramPacket sendPacket = new DatagramPacket(data, data.length, broadcastAddress,
                     6664);
 
+            _log.debug("Sending broadcast message to %s", broadcastAddress.toString());
             sendSocket.send(sendPacket);
+            _log.debug("Waiting for omnipy to respond");
             listenSocket.receive(listenPacket);
-
+            _log.debug("Received a response of length %d from %s", receive.length,
+                    listenPacket.getAddress().toString());
             byte[] received = listenPacket.getData();
             byte[] compare = "wut".getBytes(StandardCharsets.US_ASCII);
             if (received.length == 3 && received[0] == compare[0] &&
                 received[1] == compare[1] && received[2] == compare[2])
             {
-                address = listenPacket.getAddress().getHostAddress();
+                _log.debug("Response match for omnipy");
+                address = listenPacket.getAddress().toString();
+            }
+            else
+            {
+                _log.debug("Response is not what we expected");
             }
 
         } catch (SocketException e) {
