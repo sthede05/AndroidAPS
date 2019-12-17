d4ddf2a9b2debcd46b0fecc3deebdafd5af1beb3
Barış Kurtlutepe <barisk@gmail.com>
2019-12-18 00:54:50 +0100

    disable remote detonation

--- app/src/main/java/info/nightscout/androidaps/utils/FabricPrivacy.java
+++ app/src/main/java/info/nightscout/androidaps/utils/FabricPrivacy.java
@@ -14,7 +14,7 @@ import info.nightscout.androidaps.BuildConfig;
 import info.nightscout.androidaps.MainApp;
 import info.nightscout.androidaps.logging.L;
 import info.nightscout.androidaps.plugins.configBuilder.ConfigBuilderPlugin;
-import info.nightscout.androidaps.plugins.constraints.signatureVerifier.SignatureVerifierPlugin;
+//import info.nightscout.androidaps.plugins.constraints.signatureVerifier.SignatureVerifierPlugin;
 
 /**
  * Created by jamorham on 21/02/2018.
@@ -128,9 +128,9 @@ public class FabricPrivacy {
         MainApp.getFirebaseAnalytics().setUserProperty("Version", BuildConfig.VERSION);
         MainApp.getFirebaseAnalytics().setUserProperty("HEAD", BuildConfig.HEAD);
         MainApp.getFirebaseAnalytics().setUserProperty("Remote", remote);
-        List<String> hashes = SignatureVerifierPlugin.getPlugin().shortHashes();
-        if (hashes.size() >= 1)
-            MainApp.getFirebaseAnalytics().setUserProperty("Hash", hashes.get(0));
+//        List<String> hashes = SignatureVerifierPlugin.getPlugin().shortHashes();
+//        if (hashes.size() >= 1)
+//            MainApp.getFirebaseAnalytics().setUserProperty("Hash", hashes.get(0));
 
         if (ConfigBuilderPlugin.getPlugin().getActivePump() != null)
             MainApp.getFirebaseAnalytics().setUserProperty("Pump", ConfigBuilderPlugin.getPlugin().getActivePump().getClass().getSimpleName());
