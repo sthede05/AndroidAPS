c8d53f26fc29a3595bf040e836770921c5d60cb4
jhaydraude <jhaydraude@salesforce.com>
2020-01-12 14:02:45 -0500

    add en_GB locale

--- app/src/main/java/info/nightscout/androidaps/utils/DateUtil.java
+++ app/src/main/java/info/nightscout/androidaps/utils/DateUtil.java
@@ -108,14 +108,14 @@ public class DateUtil {
     }
 
     public static String dateString(Date date) {
-     //   DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
-        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM,Locale.getDefault());
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
+//        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM,Locale.getDefault());
         return df.format(date);
     }
 
     public static String dateString(long mills) {
-       // DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
-        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM,Locale.getDefault());
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
+//        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM,Locale.getDefault());
         return df.format(mills);
     }
 
